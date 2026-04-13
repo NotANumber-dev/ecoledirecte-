2da472085cba9915178f818b685c805065a2bb52
n00dle <alexis.boisse35+1234@gmail.com>
2026-04-13 09:05:56 +0200

    Update get_token.js

--- get_token.js
+++ get_token.js
@@ -1,6 +1,8 @@
-var credentials = JSON.parse(sessionStorage.getItem("credentials"));
-var token = credentials.payload.authToken;
-var accounts = JSON.parse(sessionStorage.getItem("accounts"));
-var id = accounts.payload.accounts[0].id;
-
-completion("token: " + token + " ; credentials: " + credentials + " ; accounts: " + accounts + " ; id: " + id);
+async function getToken {
+  var credentials = JSON.parse(sessionStorage.getItem("credentials"));
+  var token = credentials.payload.authToken;
+  var accounts = JSON.parse(sessionStorage.getItem("accounts"));
+  var id = accounts.payload.accounts[0].id;
+}
+getToken();
+completion(token);
