ad1c31367026d19f94a66f33178e09a825ed9325
n00dle <alexis.boisse35+1234@gmail.com>
2026-04-13 09:03:51 +0200

    Update and rename ecoledirecte-x.js to get_token.js

---
+++ get_token.js
@@ -0,0 +1,6 @@
+var credentials = JSON.parse(sessionStorage.getItem("credentials"));
+var token = credentials.payload.authToken;
+var accounts = JSON.parse(sessionStorage.getItem("accounts"));
+var id = accounts.payload.accounts[0].id;
+
+completion("token: " + token + " ; credentials: " + credentials + " ; accounts: " + accounts + " ; id: " + id);
