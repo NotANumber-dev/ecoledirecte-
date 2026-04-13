9e79299c54c2056d8df792397be8a9a3606ec663
n00dle <alexis.boisse35+1234@gmail.com>
2026-04-13 17:35:13 +0200

    chore: ajouter commentaire sur get_token.js

--- get_token.js
+++ get_token.js
@@ -1,3 +1,9 @@
+/* ================================================
+   Script pour obtenir un token API ED
+   Executer avec Raccourcis
+   https://github.com/NotANumber-dev/ecoledirecte-/
+================================================ */
+
 var credentials = JSON.parse(sessionStorage.getItem("credentials"));
 var token = credentials.payload.authToken;
 var accounts = JSON.parse(sessionStorage.getItem("accounts"));
